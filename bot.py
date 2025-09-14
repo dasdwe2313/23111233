63da303f449ab92f17c65ea022b1c32b849b91ed
dasdwe2313 <72771414+dasdwe2313@users.noreply.github.com>
2025-09-14 00:00:46 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import os
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+
 TOKEN = os.getenv("OTA0MDY2MTc2MDk3ODQ5NDM0.GnBXYm.UesJD5wHakobg8d3howHMuO09lOOaT9F-4JfkQ")
-bot = commands.Bot(command_prefix="!", intents=commands.Intents.all())
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():

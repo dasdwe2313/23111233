39269ec1c87e9a17f61bc38b04c4baec0a509bb9
dasdwe2313 <72771414+dasdwe2313@users.noreply.github.com>
2025-09-14 01:27:34 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv  # opcional, útil para testes locais
+from dotenv import load_dotenv  # útil para testes locais
 
-load_dotenv()  # carrega variáveis de .env se estiver rodando localmente
+load_dotenv()  # carrega variáveis do arquivo .env se existir
 
-TOKEN = os.getenv("3Ch_x_kjwhG5Z4CpRAck2UDNXHvpBbi1")  # <- aqui está o nome correto da variável!
+# Pega o valor da variável de ambiente DISCORD_TOKEN
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)

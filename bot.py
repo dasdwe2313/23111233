b3dcd2644f708ac25dba0d629ea81134dc2d2933
dasdwe2313 <72771414+dasdwe2313@users.noreply.github.com>
2025-09-14 00:22:11 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-TOKEN = os.getenv("OTA0MDY2MTc2MDk3ODQ5NDM0.GnBXYm.UesJD5wHakobg8d3howHMuO09lOOaT9F-4JfkQ")
 intents = discord.Intents.all()
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -15,7 +12,12 @@ async def on_ready():
     print(f"✅ Bot conectado como {bot.user}")
 
 initial_extensions = ['cogs.music']
-for ext in initial_extensions:
-    bot.load_extension(ext)
 
-bot.run(TOKEN)
+import asyncio
+
+async def main():
+    for ext in initial_extensions:
+        await bot.load_extension(ext)
+    await bot.start(TOKEN)
+
+asyncio.run(main())
